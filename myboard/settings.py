3fa9c2d685843b365b19cd024c3f64882c958550
jeongwwon <spwldmlgla@naver.com>
2023-05-19 19:51:23 +0900

    jeongs

--- myboard/settings.py
+++ myboard/settings.py
@@ -28,7 +28,7 @@ SECRET_KEYgit  = os.environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".herokuapp.com",'127.0.0.1']
+ALLOWED_HOSTS = ["triplei.herokuapp.com",'127.0.0.1']
 
 
 # Application definition
