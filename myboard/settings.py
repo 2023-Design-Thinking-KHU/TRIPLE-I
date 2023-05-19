4aff794981fe100aca597b51418ac25ab40ff047
jeongwwon <spwldmlgla@naver.com>
2023-05-19 19:36:21 +0900

    jeongwon

--- myboard/settings.py
+++ myboard/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 import dj_database_url
 import environ
+
 env=environ.Env()
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
