9ac0c751a49379b8db7b909d2121244841e3253d
jeongwwon <spwldmlgla@naver.com>
2023-05-27 20:55:25 +0900

    123

--- users/models.py
+++ users/models.py
@@ -7,15 +7,14 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Profile(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     username = models.CharField(max_length=128, default='default_username')
-    email= models.CharField(max_length=128)
-    propensity=models.CharField(max_length=128,blank=True)
-    
+    email = models.CharField(max_length=128)
+    propensity = models.CharField(max_length=128, blank=True)
 
-@receiver(post_save,sender=User)
-def create_user_profile(sender,instance,created,**kwargs):
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
-          username = instance.username
-          email = instance.email
-          Profile.objects.create(user=instance, username=username, email=email)
+        username = instance.username
+        email = instance.email
+        Profile.objects.create(user=instance, username=username, email=email)
