188a9f242fc0b6414da3c17280b9a5e1ad1cd102
jeongwwon <spwldmlgla@naver.com>
2023-05-27 20:40:31 +0900

    123

--- triplei/urls.py
+++ triplei/urls.py
@@ -21,4 +21,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
