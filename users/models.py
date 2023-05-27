adf9a125abf572e38f020f92b5d1917e8c68fa65
jeongwwon <spwldmlgla@naver.com>
2023-05-27 19:35:55 +0900

    123

--- users/models.py
+++ users/models.py
@@ -8,11 +8,14 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
-    nickname=models.CharField(max_length=128)
-    propensity=models.CharField(max_length=128)
-    image=models.ImageField(default='default.PNG')
+    username=models.CharField(max_length=128)
+    email= models.CharField(max_length=128)
+    propensity=models.CharField(max_length=128,blank=True)
+    
 
 @receiver(post_save,sender=User)
 def create_user_profile(sender,instance,created,**kwargs):
     if created:
-        Profile.objects.create(user=instance)
+          username = instance.username
+          email = instance.email
+          Profile.objects.create(user=instance, username=username, email=email)
