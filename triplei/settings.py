80ffff0902cc4f6b783c1ea2e629930079c42f8a
jeongwwon <spwldmlgla@naver.com>
2023-05-28 14:10:15 +0900

    123

--- triplei/settings.py
+++ triplei/settings.py
@@ -11,11 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-from django.http import HttpResponse
+
 import dj_database_url
 import environ
 
-RESPONSE_MIME_TYPE = 'text/css'
 env=environ.Env()
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,13 +28,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com",'127.0.0.1']
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static', 'dist'),
+    os.path.join(BASE_DIR,'dist'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    
@@ -144,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/dist'
+STATIC_URL = '/dist'
 
 
 MEDIA_URL='/media/'
