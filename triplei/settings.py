e5c1a767613f10e2509fcaa23162fa2a8a7860fe
jeongwwon <spwldmlgla@naver.com>
2023-05-28 11:48:31 +0900

    Integrate

--- triplei/settings.py
+++ triplei/settings.py
@@ -32,6 +32,21 @@ DEBUG = False
 ALLOWED_HOSTS = [".herokuapp.com",'127.0.0.1']
 
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'dist'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'dist'),
+        ],
+        # ...
+    },
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -64,21 +79,7 @@ CORS_ALLOW_CREDENTIALS=True
 
 ROOT_URLCONF = 'triplei.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+
 
 WSGI_APPLICATION = 'triplei.wsgi.application'
 
@@ -141,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
