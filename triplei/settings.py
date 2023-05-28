29a9b31a39d68f6a8d93577ae7a7b1c823e82222
jeongwwon <spwldmlgla@naver.com>
2023-05-28 14:17:37 +0900

    !23

--- triplei/settings.py
+++ triplei/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/dist'
+STATIC_URL = '/static/'
 
 
 MEDIA_URL='/media/'
