bd573cf0d0f90ee475df65d92dcf42f651d92845
jeongwwon <spwldmlgla@naver.com>
2023-06-02 11:22:18 +0900

    123

--- triplei/urls.py
+++ triplei/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
-from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
