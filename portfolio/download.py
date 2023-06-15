c57bf5188914827e190e25c074e6a4d59bef7875
jeongwwon <spwldmlgla@naver.com>
2023-06-15 22:20:57 +0900

    123

--- portfolio/download.py
+++ portfolio/download.py
@@ -1,22 +1,18 @@
 from django.http import FileResponse
 from django.conf import settings
-import zipfile
 import os
 
 def download_weights_csv(request):
-    file_paths = [
-        os.path.join(settings.MEDIA_ROOT, 'weights.csv'),
-        os.path.join(settings.MEDIA_ROOT, 'chart.png'),
-    ]
-    zip_file_path = os.path.join(settings.MEDIA_ROOT, 'download.zip')
+    weights_file_path = os.path.join(settings.MEDIA_ROOT, 'weights.csv')
+    chart_file_path = os.path.join(settings.MEDIA_ROOT, 'chart.png')
     
-    with zipfile.ZipFile(zip_file_path, 'w') as zip_file:
-        for file_path in file_paths:
-            if os.path.exists(file_path):
-                zip_file.write(file_path, os.path.basename(file_path))
-    
-    if os.path.exists(zip_file_path):
-        with open(zip_file_path, 'rb') as file:
-            response = FileResponse(file, as_attachment=True, content_type='application/zip')
-            response['Content-Disposition'] = 'attachment; filename="download.zip"'
+    if os.path.exists(weights_file_path) and os.path.exists(chart_file_path):
+        with open(weights_file_path, 'rb') as weights_file, open(chart_file_path, 'rb') as chart_file:
+            response = FileResponse()
+            response['Content-Disposition'] = 'attachment; filename="weights_and_chart.zip"'
+            response['Content-Type'] = 'application/zip'
+            
+            response.write(weights_file.read())
+            response.write(chart_file.read())
+            
             return response
